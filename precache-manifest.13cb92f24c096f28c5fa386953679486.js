15e69474722d5d825c4881f4322e271fdef6f358
4n5hu <anshusaini189381@gmail.com>
2023-01-21 04:01:04 +0530

    Updates

---
+++ precache-manifest.13cb92f24c096f28c5fa386953679486.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ba48eaff6bd7255dcb1",
+    "url": "/weathy/static/css/main.13b7c60d.chunk.css"
+  },
+  {
+    "revision": "5ba48eaff6bd7255dcb1",
+    "url": "/weathy/static/js/main.5ba48eaf.chunk.js"
+  },
+  {
+    "revision": "975f63afe5057efb9a20",
+    "url": "/weathy/static/js/1.975f63af.chunk.js"
+  },
+  {
+    "revision": "a39879888cd277db98bb",
+    "url": "/weathy/static/js/2.a3987988.chunk.js"
+  },
+  {
+    "revision": "af19421030479d118a02",
+    "url": "/weathy/static/js/runtime~main.af194210.js"
+  },
+  {
+    "revision": "a1b946b77451176dd46264016872c05e",
+    "url": "/weathy/static/media/404.a1b946b7.png"
+  },
+  {
+    "revision": "97ee77b14ccbc72abb7d1af4aeb1c763",
+    "url": "/weathy/static/media/clear.97ee77b1.png"
+  },
+  {
+    "revision": "2a1f869aaed98462ff06177e2fb3288e",
+    "url": "/weathy/static/media/snow.2a1f869a.png"
+  },
+  {
+    "revision": "08225153a80c8c7b577838e24dec4c63",
+    "url": "/weathy/static/media/cloud.08225153.png"
+  },
+  {
+    "revision": "9709df310feb062525f39929f2ddb88f",
+    "url": "/weathy/static/media/mist.9709df31.png"
+  },
+  {
+    "revision": "4789ada3f3f28c1550e7dae4be17e975",
+    "url": "/weathy/static/media/rain.4789ada3.png"
+  },
+  {
+    "revision": "40e36ca325a8d3c36cf700b19f978da0",
+    "url": "/weathy/index.html"
+  }
+];
